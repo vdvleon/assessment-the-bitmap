ec7cbdffbf6f710e3ec2c3eab7b8ca16d00ee060
Leon van der Veen <leon.vanderveen@bloomon.nl>
2019-07-16 15:31:56 +0200

    chore/refactor: run linter and fix problems in Bitmap

--- src/Bitmap/Bitmap.test.ts
+++ src/Bitmap/Bitmap.test.ts
@@ -1,51 +1,51 @@
-import Bitmap from './Bitmap';
+import Bitmap from "./Bitmap";
 
-describe('Bitmap', () => {
-    describe('constructor', () => {
-        it('should create a bitmap with a data matrix for given size', () => {
+describe("Bitmap", () => {
+    describe("constructor", () => {
+        it("should create a bitmap with a data matrix for given size", () => {
             const width = 2;
             const height = 3;
             const bitmap = new Bitmap<boolean>(width, height, false);
 
-            expect(bitmap.data).toEqual(height);
+            expect(bitmap.getData().length).toEqual(height);
             for (let h = 0; h < height; ++h) {
-                expect(bitmap.data[h].length).toEqual(width);
+                expect(bitmap.getData()[h].length).toEqual(width);
             }
         });
 
-        it('should create a bitmap with a data matrix of given type', () => {
-            const bitmap1 = new Bitmap<string>(1, 1, 'foobar');
-            expect(typeof bitmap1.data[0][0]).toBe('string');
+        it("should create a bitmap with a data matrix of given type", () => {
+            const bitmap1 = new Bitmap<string>(1, 1, "foobar");
+            expect(typeof bitmap1.getData()[0][0]).toBe("string");
 
             const bitmap2 = new Bitmap<number>(1, 1, 13.37);
-            expect(typeof bitmap2.data[0][0]).toBe('number');
+            expect(typeof bitmap2.getData()[0][0]).toBe("number");
 
             const bitmap3 = new Bitmap<Date>(1, 1, new Date(0));
-            expect(bitmap3.data[0][0]).toBeInstanceOf(Date);
+            expect(bitmap3.getData()[0][0]).toBeInstanceOf(Date);
         });
     });
 
-    describe('setValueAt', () => {
-       it('should fail when given an incorrect coordinate', () => {
-          const bitmap = new Bitmap<boolean>(1, 1, false);
-          expect(() => bitmap.setValueAt(1, 1, true)).toThrow(Error);
-       });
+    describe("setValueAt", () => {
+        it("should fail when given an incorrect coordinate", () => {
+            const bitmap = new Bitmap<boolean>(1, 1, false);
+            expect(() => bitmap.setValueAt(1, 1, true)).toThrow(Error);
+        });
 
-        it('should set value for a valid coordinate and return this', () => {
+        it("should set value for a valid coordinate and return this", () => {
             const bitmap = new Bitmap<boolean>(1, 1, false);
 
             expect(bitmap.setValueAt(0, 0, true)).toBe(bitmap);
-            expect(bitmap.data[0][0]).toBe(true);
+            expect(bitmap.getData()[0][0]).toBe(true);
         });
     });
 
-    describe('getValueAt', () => {
-        it('should fail when given an incorrect coordinate', () => {
+    describe("getValueAt", () => {
+        it("should fail when given an incorrect coordinate", () => {
             const bitmap = new Bitmap<boolean>(1, 1, false);
             expect(() => bitmap.getValueAt(1, 1)).toThrow(Error);
         });
 
-        it('should get value for a valid coordinate', () => {
+        it("should get value for a valid coordinate", () => {
             const bitmap = new Bitmap<boolean>(1, 1, false);
 
             expect(bitmap.getValueAt(0, 0)).toBe(false);
--- src/Bitmap/Bitmap.ts
+++ src/Bitmap/Bitmap.ts
@@ -1,9 +1,9 @@
-import InvalidCoordinateError from './InvalidCoordinateError';
+import InvalidCoordinateError from "./InvalidCoordinateError";
 
 export default class Bitmap<T> {
-    width: number;
-    height: number;
-    data: T[][];
+    private width: number;
+    private height: number;
+    private data: T[][];
 
     /**
      * Initialize a bitmap for a given size.
@@ -12,7 +12,7 @@ export default class Bitmap<T> {
      * @param height
      * @param initialValue
      */
-    constructor(width: number, height: number, initialValue: T) {
+    public constructor(width: number, height: number, initialValue: T) {
         this.width = width;
         this.height = height;
         this.data = [];
@@ -26,7 +26,19 @@ export default class Bitmap<T> {
         }
     }
 
-    setValueAt(x: number, y: number, value: T): this {
+    public getWidth(): number {
+        return this.width;
+    }
+
+    public getHeight(): number {
+        return this.height;
+    }
+
+    public getData(): T[][] {
+        return this.data;
+    }
+
+    public setValueAt(x: number, y: number, value: T): this {
         this.validateCoordinate(x, y);
 
         this.data[y][x] = value;
@@ -34,13 +46,13 @@ export default class Bitmap<T> {
         return this;
     }
 
-    getValueAt(x: number, y: number): T {
+    public getValueAt(x: number, y: number): T {
         this.validateCoordinate(x, y);
 
         return this.data[y][x];
     }
 
-    validateCoordinate(x, y) {
+    private validateCoordinate(x, y) {
         if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
             throw new InvalidCoordinateError(x, y, this.width, this.height);
         }
